23818f8faf69f6b8d4ac4c516e673e36ad1d12ea
John Kananda <jkananda5@outlook.com>
2025-06-27 14:21:05 -0600

    initial commit with image set to png and optimized

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import "./globals.css";
+import "./styles/globals.css";
 
 export const metadata = {
   title: "Homecoming Ceremony",
